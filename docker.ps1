fbbbd51932461e42b44c6f8936286f285499e703
Michael Porter <53201842+Squidkiller01@users.noreply.github.com>
2025-02-05 12:40:20 -0500

    attempt with my own script

--- docker.ps1
+++ docker.ps1
@@ -5,24 +5,32 @@ if (-Not $elevated) {
     Exit
 }
 
-# Enable required Windows features
-Write-Host "Installing required Windows features..." -ForegroundColor Yellow
-Install-WindowsFeature -Name Containers -Restart:$false
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart:$false
+# Disables the download progress bar
+# This significantly speeds up download speeds
+$ProgressPreference = 'SilentlyContinue'
+Write-Host "Disabled progress bar"
 
-# Install DockerMsftProvider
-Write-Host "Installing Docker Provider..." -ForegroundColor Yellow
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-
-# Install Docker
-Write-Host "Installing Docker..." -ForegroundColor Yellow
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-
-# Restart the system to apply changes
-Write-Host "Installation complete. A restart is required for Docker to function properly." -ForegroundColor Green
-$restart = Read-Host "Do you want to restart now? (Y/N)"
-if ($restart -match "[Yy]") {
-    Restart-Computer -Force
+# Install the DockerMsftProvider Module
+Write-Host "Installing DockerMsftProvider Module"
+Install-Module DockerMsftProvider -Force
+if (Get-Module -ListAvailable -Name DockerMsftProvider) {
+	Write-Host "Module Successfully Installed"
 } else {
-    Write-Host "Please restart your server manually to complete the installation." -ForegroundColor Yellow
+	Write-Host "Module Installation Failed"
 }
+
+# Download the Docker Install Script
+Write-Host "Downloading Docker Install Script"
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+$path = "C:\Users\Administrator"
+if ([System.IO.File]::Exists($path)) {
+	Write-Host "Docker Script Installed"
+	
+	#Execute the Script
+	C:\Users\Administrator\install-docker-ce.ps1
+	
+} else {
+	Write-Host "Docker Script Installation Failed"
+	Exit
+}
+
