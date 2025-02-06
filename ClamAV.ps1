fb0988fe7bace78f7184bf259bd32be23dc2e1e0
Michael Porter <53201842+mporter012@users.noreply.github.com>
2025-02-06 10:12:06 -0500

    Update ClamAV.ps1

--- ClamAV.ps1
+++ ClamAV.ps1
@@ -1,4 +1,4 @@
-﻿# Ensure the script is running as Administrator
+# Ensure the script is running as Administrator
 $elevated = [bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 if (-Not $elevated) {
     Write-Host "Please run this script as an Administrator!" -ForegroundColor Red
@@ -33,16 +33,44 @@ Write-Host "Updating ClamAV Virus Database"
 
 # Define the path to the freshclam.conf file
 $freshClamConfigPath = "C:\Program Files\ClamAV\freshclam.conf"
+$clamDatabaseConfigPath = "C:\Program Files\ClamAV\clamd.conf"
 
-if(-not (Test-Path $freshClamConfigPath)){
-    Write-Host "FreshClam Config not found"
-    Write-Host "Generating new config"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/aosm/clamav/refs/heads/master/clamav.Conf/freshclam.conf.default" -OutFile "$freshClamConfigPath"
-    if (Test-Path $freshClamConfigPath){
-        Write-Host "FreshClam Config Successfully Generated"
-    }else{
-        Write-Host "FreshClam Config Unable to Generate"
+# If freshclam.conf does not exist, re-creates the file from the config examples
+if (-not (TestPath $freshClamConfigPath)){
+    Write-Host "FreshClam Config Not Found. Generating New Config"
+    copy C:\Program Files\conf_exmaples\freshclam.conf.sample $freshClamConfigPath
+    Write-Host "Config Generated."
+    #Write-Host "Delete The Line That Says "Example" on Line 9. Then Save & Continue" -ForegroundColor Yellow
+    #write-exe .\freshclam.conf -Wait
+    Write-Host "Modifying the freshclam.conf file..." -ForegroundColor Green
+    $fileContent = Get-Content $freshClamConfigPath
+    #Remove the line containing "Example" and uncomment the UpdateLogFile line
+    $fileContent = $fileContent | ForEach-Object {
+        #Remove the line containing 'Example'
+        if ($_ -match "Example"){
+            $null #Exclude this line
+        }
+        # Uncomment the UpdateLogfile line
+        elseif ($_ -match "^#\s*UpdateLogFile") {
+            $_ -replace "^#\s*", ""
+        }
+        else {
+            $_
+        }
     }
+    #Write the modified content back to the file
+    $fileContent | Set-Content $freshClamConfigPath
+
+    Write-Host "freshclam.conf has been modified successfully." -ForegroundColor Green
+}
+
+# If clamd.conf does not exist, re-creates the file from the config samples
+if (-not (TestPath $clamDatabaseConfigPath)){
+    Write-Host "FreshClam Config Not Found. Generating New Config"
+    copy C:\Program Files\conf_exmaples\clamd.conf.sample $clamDatabaseConfigPath
+    Write-Host "Config Generated. Opening Config"
+    Write-Host "Delete The Line That Says "Example" on Line 9. Then Save & Continue" -ForegroundColor Yellow
+    write-exe .\clamd.conf -Wait
 }
 
 # Run an automatic scan
