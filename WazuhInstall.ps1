7c4b37dba4c59e8e9175eae6c63a97629d093db6
Michael Porter <53201842+mporter012@users.noreply.github.com>
2025-02-06 15:32:48 -0500

    Update WazuhInstall.ps1

--- WazuhInstall.ps1
+++ WazuhInstall.ps1
@@ -1,7 +1,22 @@
-﻿﻿# TODO: Add WAZUH_AGENT_GROUP value to the CLI command call
-
 # Download the most recent version of the Wazuh windows agent
-Invoke-WebRequest -Uri "https://packages.wazuh.com/4.x/windows/wazuh-agent-4.9.2-1.msi" -Outfile C:\Users\ccdc\wazuh-agent-4.9.2-1.msi
+Invoke-WebRequest -Uri "https://packages.wazuh.com/4.x/windows/wazuh-agent-4.9.2-1.msi" -Outfile "$env:USERPROFILE\wazuh-agent-4.9.2-1.msi"
 
 # Install the agent with all options presented.
-C:\Users\ccdc\wazuh-agent-4.9.2-1.msi /q WAZUH_MANAGER="172.20.241.20" WAZUH_MANAGER_PORT="514" WAZUH_PROTOCOL="UDP" 
+& "$env:USERPROFILE\wazuh-agent-4.9.2-1.msi" /q WAZUH_MANAGER="172.20.241.20" WAZUH_MANAGER_PORT="514" WAZUH_PROTOCOL="UDP" 
+
+# Check if the Wazuh Agent service exists
+$Service = Get-Service -Name WazuhSvc -ErrorAction SilentlyContinue
+
+if ($Service) {
+    # If the service exists, check its status
+    if ($Service.Status -eq "Stopped"){
+        Write-Host "Wazuh Agent service is stopped. Starting it now..." -ForegroundColor Yellow
+        Start-Service -Name WazuhSvc
+        Write-Host "Wazuh Agent service started successfully." -ForegroundColor Green
+    }
+    elseif ($Service.Status -eq "Running") {
+        Write-Host "Wazuh Agent is already running." -ForegroundColor Cyan
+    }
+} else {
+    Write-Host "Wazuh Agent service is NOT installed!" -ForegroundColor Red
+}
