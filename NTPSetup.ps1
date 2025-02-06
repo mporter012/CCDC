2d1ecd10270cdad5db2c58466248cd2b312d3bfc
Michael Porter <53201842+mporter012@users.noreply.github.com>
2025-02-05 19:39:39 -0500

    Update NTPSetup.ps1

--- NTPSetup.ps1
+++ NTPSetup.ps1
@@ -9,12 +9,13 @@ $updateToNameServer = "time-a-b.nist.gov"
 
 Write-Host "Current NTPServer: $currentNTPServer"
 
-# Update the NTP to point to the internal Debian machine
-# Update "[placeholder]" to point to the NTP NameServer you want the NTP to point to
-w32tm /config /syncfromflags:manual /manualpeerlist: /manualpeerlist: "$updateToNameServer" /update
+#Update the NTP to point to the specified NTP NameServer
+w32tm /config /syncfromflags:manual /manualpeerlist:"$updateToNameServer" /update > $null 2>&1
 
-# Restart the w32time service
+#Restart the w32time Service
 Restart-Service w32time
 Write-Host "w32time Service Restarted"
 
-Write-Host "Current NTPServer: $currentNTPServer"
+#Verify and display the new NTPServer
+$newNTPServer = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters").NtpServer
+Write-Host "New NTPServer: $newNTPServer"
