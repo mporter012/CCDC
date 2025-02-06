58b960678d2bf48f440e63554dedf009cd34350e
Michael Porter <53201842+mporter012@users.noreply.github.com>
2025-02-06 14:15:10 -0600

    Add files via upload

---
+++ WazuhInstall.ps1
@@ -0,0 +1,7 @@
+﻿﻿# TODO: Add WAZUH_AGENT_GROUP value to the CLI command call
+
+# Download the most recent version of the Wazuh windows agent
+Invoke-WebRequest -Uri "https://packages.wazuh.com/4.x/windows/wazuh-agent-4.9.2-1.msi" -Outfile C:\Users\ccdc\wazuh-agent-4.9.2-1.msi
+
+# Install the agent with all options presented.
+C:\Users\ccdc\wazuh-agent-4.9.2-1.msi /q WAZUH_MANAGER="172.20.241.20" WAZUH_MANAGER_PORT="514" WAZUH_PROTOCOL="UDP" 
